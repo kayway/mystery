7a7e4ac812a7f6d8bedda317b22691a42b087af4
kayway <eeu444@bangor.ac.uk>
2015-08-10 16:15:14 +0100

    files

--- bin/game1/evidence/check-letter.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-if [[ $1 = "01-05-02-06-03-08-07-04-10-09-12-14-13-11-15-17-19-16-18" ]];
-then
-	echo Correct here is the letter
-	cat Solution/letter.txt
-else
-	echo Incorrect!!
-fi
